92c6ae4c4a3e0a8ac3450a5922f7990ef70823fb
MartinMousa <martin.mousa88@gmail.com>
2023-10-31 12:00:32 -0700

    last tasks test 2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for digit1 in range(0, 9):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8:
-            print("{:d}{:d}".format(digit1, digit2))
-            break
-        print("{:d}{:d}".format(digit1, digit2), end=", ")
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i != j:
+            print("{}{}".format(i, j), end=", " if i < 8 else "\n")
