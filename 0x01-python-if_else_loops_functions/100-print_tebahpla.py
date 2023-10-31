bc271814682202422a25c96fe651322a480f2d28
MartinMousa <martin.mousa88@gmail.com>
2023-10-31 12:02:49 -0700

    last tasks test 3

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,26 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-
-listint_t *insert_node(listint_t **head, int number);
-
-#endif /* LISTS_H */
+#!/usr/bin/python3
+for i in range(-122, -96):
+    print("{}".format(chr((-32-i) if (-i) % 2 != 0 else (-i))), end="")
